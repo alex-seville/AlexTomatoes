9a7657f312b159070e944081cc5dcc609afb6b74
seville <seville@yahoo-inc.com>
2014-03-14 19:34:07 -0700

    Initial Commit

---
+++ RottenTomatoes/ASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASAppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Alexander Seville on 3/14/14.
+//  Copyright (c) 2014 Alexander Seville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
